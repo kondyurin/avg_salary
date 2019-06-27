c6380cc9751e2931d171208a19946ae0685b7826
kondyurin <a.kondyurin@yandex.ru>
2019-06-27 21:28:00 +0300

    Change func

--- main.py
+++ main.py
@@ -21,31 +21,54 @@ def predict_rub_salary(vacancy):
     return salary
     
 
-def get_hh_vacancy():
-    result = []
-    lang_salary = dict()
-    langs = ['python', 'java']
+def get_hh_vacancy_data(lang):
     url = "https://api.hh.ru/vacancies"
-    for lang in langs:
-        path = {
-            'text': 'программист {}'.format(lang),
-            'area': 1,
-            'period': 30
+    path = {
+        'text': 'программист {}'.format(lang),
+        'area': 1,
+        'period': 30
+    }
+    data = fetch_json(url, path)
+    return data
+
+
+def get_hh_lang_info(hh_vacancy_data):
+    vacancies_count = hh_vacancy_data['found']
+    lang_info = {
+        lang: {
+            'vacancies_found': vacancies_count, 
+            'vacancies_processed':'', 
+            'average_salary':''
         }
-        data = fetch_json(url, path)
-        lang_salary[lang] = data
-    return lang_salary
-
-
-def get_salary_info_hh(hh_vacancies):
-    langs_info = dict()
-    for k,v in hh_vacancies.items():
-        vacancies_count = v['found']
-        vacancies_data = v['items']
-        for vacancy in vacancies_data:
-            avg_salary = predict_rub_salary(vacancy['salary']))
+    }
+    return lang_info
 
 
 if __name__ == "__main__":
-    hh_vacancies = get_hh_vacancy()
-    get_salary_info_hh(hh_vacancies)
+
+    langs = ['python', 'java']
+    for lang in langs:
+        hh_vacancies = get_hh_vacancy_data(lang)
+        res = get_hh_lang_info(hh_vacancies)
+        print(res)
+        
+
+
+
+# {
+#     "Python": { 
+#         "vacancies_found": 1068,
+#         "vacancies_processed": 13,
+#         "average_salary": 123853
+#     },
+#     "Java": {
+#         "vacancies_found": 1592,
+#         "vacancies_processed": 16,
+#         "average_salary": 121456
+#     },
+#     "Javascript": { 
+#         "vacancies_found": 3696,
+#         "vacancies_processed": 18,
+#         "average_salary": 140173
+#     }
+# }
